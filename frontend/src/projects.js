277449947a50922a18e30666f29a97a7caada18a
hotcoffeehero <hotcoffeehero@gmail.com>
2021-05-09 17:54:34 +0900

    Trav 5050: Project 1: Image Rolex

--- frontend/src/projects.js
+++ frontend/src/projects.js
@@ -3,28 +3,35 @@ const people = '/images/people/'
 
 const projects = [
   {
-    id: '1',
+    // id: '1',
     url: 'https://proshop-alpha.herokuapp.com/',
     image: `${project}proshop.png`,
     title: 'PROSHOP',
     desc: 'A MERN stack Ecommerce site. A code-along with Traversy Media',
   },
   {
-    id: '2',
+    // id: '11',
+    url: 'http://hateful-trains.surge.sh/',
+    image: `${project}child_of_god.png`,
+    title: 'Image Rolex',
+    desc: 'Trav: Click on an image, watch it grow. Japan is カッコイイね。',
+  },
+  {
+    // id: '2',
     url: 'http://purring-beef.surge.sh/',
     image: `${project}currency_converter.png`,
     title: 'Forex Calculator',
-    desc: 'ATraversy code-along. My first forex calculator with Fetch API!',
+    desc: 'A Traversy code-along. My first forex calculator with Fetch API!',
   },
   {
-    id: '3',
+    // id: '3',
     url: 'https://manchester-city-46483.web.app/',
     image: `${project}man_city.png`,
     title: 'Man City',
     desc: 'A FERN stack homepage for the famous football club.',
   },
   {
-    id: '4',
+    // id: '4',
     url: 'http://thoughtful-match.surge.sh/',
     image: `${project}movie_booker.png`,
     title: '映画を予約しよう！',
@@ -32,21 +39,21 @@ const projects = [
   },
 
   {
-    id: '7',
+    // id: '7',
     url: 'http://curved-value.surge.sh/',
     image: `${project}array_methods.png`,
     title: 'Array Methods',
     desc: 'Code with Trav. DOM manipulation with array methods in Vanilla JS.',
   },
   {
-    id: '7',
+    // id: '7',
     url: 'https://www.google.com',
     image: `${people}nikola_tesla.jpg`,
     title: 'Coming Soon',
     desc: "Peruse the man we didn't learn about in school for a reason. ",
   },
   {
-    id: '8',
+    // id: '8',
     url: 'http://gabby-mark.surge.sh/',
     image: `${project}the_venue.png`,
     title: 'The Venue',
@@ -54,14 +61,14 @@ const projects = [
       'My first ever React Project with Coding Revolution. 2.0 will be Britpop related.',
   },
   {
-    id: '9',
+    // id: '9',
     url: 'http://unused-doll.surge.sh/',
     image: `${project}video_player.png`,
     title: 'Video Player',
     desc: 'A video/audio API code-along with Traversy. This is me in Nagasaki!',
   },
   {
-    id: '10',
+    // id: '10',
     url: 'http://nondescript-comfort.surge.sh/',
     image: `${project}form_validator_2.jpg`,
     title: 'Form Validator',
@@ -69,7 +76,7 @@ const projects = [
       'A code-along with Traversy Media, JS forms on html and css canvas...',
   },
   {
-    id: '11',
+    // id: '11',
     url: 'https://www.google.com',
     image: `${project}coming_soon.jpg`,
     title: 'Coming Soon',
